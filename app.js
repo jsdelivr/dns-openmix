a321c962ff58e49a1b7916cfdae3c6b3a0479b7b
Dmitriy Akulov <jimaek@hotmail.com>
2016-09-11 15:09:07 +0200

    removed tm-mg

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ var handler = new OpenmixApplication({
     providers: {
         'cloudflare': 'cdn.jsdelivr.net.cdn.cloudflare.net',
         'maxcdn': 'jsdelivr3.dak.netdna-cdn.com',
-        'tm-mg': 'tm-mg.jsdelivr.net',
         'keycdn': 'jsdelivr-cb7.kxcdn.com',
         'quantil': 'cdn.jsdelivr.net.mwcloudcdn.com'
     },
     // Use countryMapping to give consideration to additional providers for
     // specific countries:
     countryMapping: {
-        'MG': [ 'tm-mg', 'maxcdn', 'cloudflare', 'keycdn' ],
         'CN': [ 'quantil'],
         'TH': [ 'maxcdn', 'cloudflare', 'keycdn', 'quantil'],
         'BR': [ 'cloudflare']
